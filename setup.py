8998c3e6c2e830d0de2c79e364640350538cc9b0
yinfeiy <yangyin7@gmail.com>
2023-02-06 15:40:33 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from version import __version__  # pylint: disable=g-import-not-at-top
 with open('README.md') as fp:
   _LONG_DESCRIPTION = fp.read()
 
-_jax_version = '0.4.0'
+_jax_version = '0.3.25'
 _jaxlib_version = '0.1.76'
 
 setuptools.setup(
